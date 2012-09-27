469cdaf6a8089ebb2541df62e74516d612274c41
Noeljunior <devnull@localhost>
2012-09-27 19:57:35 +0100

    init

--- php/header.php
+++ php/header.php
@@ -1,4 +1,5 @@
 <?php /* Header - title (...), menu and login/logout */
+
     /* stop execution ifnot included */
     $included = strtolower(realpath(__FILE__)) != strtolower(realpath($_SERVER['SCRIPT_FILENAME']));
     if (!$included)
