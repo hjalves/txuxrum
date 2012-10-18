5f7d07e6d1dd4106b6c02e824174b22c0abe200e
Noeljunior <devnull@localhost>
2012-10-18 16:12:51 +0100

    messages

--- php/message.php
+++ php/message.php
@@ -23,6 +23,8 @@
                     <td>
                         <div class="bodyframe" width="100%">
                             
+                            
+                            <div class="messagebox">
                             <?php
                                 vf_printmessagesent("to", "ja foi ontem", "Tao? Tudo bem?");
                                 vf_printmessagesent("to", "ja foi ontem", "Tao? Tudo bem?");
@@ -32,7 +34,23 @@
                                 vf_printmessagesent("to", "ja foi ontem", "Tao? Tudo bem?");
                                 
                            ?>
-
+                            </div>
+                        
+                            
+                            <div class="msgpanel">
+                                <div class="msgpanel-filter">
+                                    <div class="msgpanel-filter-title">
+                                        Message of you and $user
+                                    </div>
+                                    <div class="msgpanel-filter-div">
+                                        <input type="text" size="20" class="msgpanel-filter-div-search" />
+                                        <input type="submit" value="filter" class="msgpanel-filter-div-button" />
+                                        <input type="submit" value="clear filter" class="msgpanel-filter-div-button" />
+                                    </div>
+                                </div>
+                            </div>    
+                            
+                        
              
                         </div>
                     </td>
