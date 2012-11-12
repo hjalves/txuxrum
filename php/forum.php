d93f9ceab79fd200456cac0fcfe804fa7d6c0326
Humberto Alves <hjalves@student.dei.uc.pt>
2012-11-12 22:51:35 +0000

    descricao nas chatrooms

--- php/forum.php
+++ php/forum.php
@@ -26,7 +26,7 @@
             }
             vf_printchatlistheader();
             while ($line = pg_fetch_row($result, null))
-                vf_printchatitem($line[0], $line[1], $line[2], $line[3], $line[4], $line[5], $line[6]);
+                vf_printchatitem($line[0], $line[1], $line[2], $line[3], $line[4], $line[5], $line[6], $line[7]);
             vf_printchatlistfooter(4, 2);
             vf_printcreatethread();
         ?>
