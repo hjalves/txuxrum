0c4f1bd3372e4f7b48ef66f678c3c8b4b69def33
Noeljunior <liamgliam@gmail.com>
2012-11-12 22:17:56 +0000

    bug: index.php

--- php/index.php
+++ php/index.php
@@ -6,35 +6,18 @@
 
 <html>
     <head>
-        <title> :: -- CHATRUM -- ::</title>
+        <title> :: -- TXUXRUM -- ::</title>
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
         <link href="gangnamstyle.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
-        <div align="center">
-            <table width="700px" border="0px" cellspacing="0px" cellpadding="0px" class="mainframe">
-                <tr>
-                    <td>
-                        <?php include("header.php"); ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <div class="bodyframe">
-
-                            Very welcome, dude!
-
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="footer">
-                        <?php vf_printfooter(); ?>
-                    </td>
-                </tr>
-            </table>
+    <div class="mainframe">
+        <div class="maintitle"> Txuxrum </div>
+        <div class="mainmenu"> <?php vf_printmainmenu(); ?> </div>
+        <div class="mainbody">
+            Very welcome, dude!
         </div>
-        
-        
+        <div class="mainfooter"> <?php vf_printfooter(); ?> </div>
+    </div>
     </body>
 </html>
