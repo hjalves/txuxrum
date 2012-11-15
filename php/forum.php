8ad2b1ff3148470e1426e4280ec09e21ab9cbf54
Noeljunior <liamgliam@gmail.com>
2012-11-15 00:48:58 +0000

    csszitos

--- php/forum.php
+++ php/forum.php
@@ -46,7 +46,6 @@
             vf_printsearchbox($user, $title);
 
             vf_printchatlistheader();
-
             while ($row = pg_fetch_row($reschatrooms, null))
                 vf_printchatitem($row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7]);
 
