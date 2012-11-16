597fb2f38f953d977f5a0f0f02e885dc9a2a4137
futuo <bernardomarques5@gmail.com>
2012-11-16 00:40:15 +0000

    default permissions

--- db/chatroom_post_trigger.sql
+++ db/chatroom_post_trigger.sql
@@ -5,12 +5,12 @@ AS $$
 
 DECLARE
     perm_rec permissions%ROWTYPE;
-
+    n_rec   BOOLEAN;
 BEGIN
 
     SELECT * INTO perm_rec FROM permissions WHERE userid = NEW.userid AND roomid = NEW.roomid;
 
-    IF (NOT perm_rec.canpost OR perm_rec.banned)
+    IF (FOUND AND (NOT perm_rec.canpost OR perm_rec.banned))
         THEN
             RAISE EXCEPTION 'user does not have permission to post';
     END IF;
@@ -23,6 +23,4 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER chatroom_post_trigger 
 BEFORE INSERT OR UPDATE OR DELETE ON messages
 FOR EACH ROW EXECUTE PROCEDURE post_permission_check();
-
-
 
