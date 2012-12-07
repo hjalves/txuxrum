3b291aabd68304c9d559547cf0372dc69b3c37a5
Humberto Alves <hjalves@student.dei.uc.pt>
2012-12-07 05:33:03 +0000

    filtra os topicos de acordo com as permissoes

--- db/create_functions.sql
+++ db/create_functions.sql
@@ -10,15 +10,16 @@ AS $$
     SELECT cr.* FROM chatrooms_lastposts cr
     LEFT JOIN (SELECT roomid, canread
                FROM permissions
-               WHERE userid = $1 ) rdperm
+               WHERE userid = $1) rdperm
     ON (cr.roomid = rdperm.roomid)
-    WHERE (coalesce(rdperm.canread, cr.readingperm) <> FALSE OR
-        cr.ownerid = $1)
+    WHERE ((coalesce(rdperm.canread, cr.readingperm, TRUE) OR
+        cr.ownerid = $1) AND ($1 <> 0 OR cr.readingperm = TRUE))
     ORDER BY coalesce(lastposttime, creationdate) DESC
 $$ LANGUAGE SQL;
 
 
 -- Efetua pesquisa às chatrooms em que user tem permissão para ler
+-- TODO: devia ser mais elaborado
 CREATE FUNCTION searchChatrooms(userid integer, title varchar, owner varchar)
 RETURNS SETOF chatrooms_lastposts
 AS $$
--- php/include/dbauth.php
+++ php/include/dbauth.php
@@ -11,7 +11,7 @@
     $dbname = "txux";
     /* user auth */
     $dbuser = "txux";
-    $dbpass = "txux";
+    $dbpass = "";
     /* verbose */
     $dbverb = FALSE;
 
