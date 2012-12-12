de0f4aa59bd1775a145ce2e5591cb8c1727f1332
futuo <bernardomarques5@gmail.com>
2012-12-12 18:06:12 +0000

    quem não tiver acesso ao tópico não pode fazer rate.

--- db/rate_trigger.sql
+++ db/rate_trigger.sql
@@ -12,14 +12,21 @@ DECLARE
 
     max_val INTEGER;
     n_rates INTEGER;
+    can_read BOOLEAN;
 
 BEGIN
 
     LOCK TABLE ratings IN EXCLUSIVE MODE;
 
+    select canread from users_permissions into can_read where userid = New.userid AND roomid = New.roomid;
+
     select COUNT(DISTINCT userid) from messages into max_val where roomid = NEW.roomid;
 
     select COUNT(DISTINCT userid) from ratings into n_rates where roomid = NEW.roomid;
+
+    IF NOT can_read THEN
+        RAISE EXCEPTION 'user has no permission to rate this chatroom';
+    END IF;
 
     IF n_rates >= max_val
         THEN
